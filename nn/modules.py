795015457dace8740e1684c6f5a8bd14e7d6e24e
Manuel Mosquera <mosquera.manuel2011@gmail.com>
2023-12-13 20:35:20 -0500

    :sparkles: Add different weight initialization strategies, create Droput layers, and add the Sequential model

--- nn/modules.py
+++ nn/modules.py
@@ -1,5 +1,7 @@
 import numpy as np
 from typing import Literal
+import math
+from abc import ABC, abstractmethod
 
 from nn.engine import Value
 
@@ -8,8 +10,16 @@ Activations = Literal['tanh', 'relu', 'sigm'] | None
 class Neuron:
 
     def __init__(self, nin, activation: Activations = None):
-        self.w = [Value(np.random.uniform(-1, 1)) for _ in range(nin)]
-        self.b = Value(np.random.uniform(-1, 1))
+        self.init_strategy =  lambda : np.random.uniform(-1, 1)
+        self.nin = nin
+
+        if activation == 'relu':
+            self.init_strategy = self._he_init_strategy(nin)
+        elif activation == 'sigm' or activation == 'tanh':
+            self.init_strategy = self._xavier_init_strategy(nin)
+
+        self.w = [Value(self.init_strategy()) for _ in range(nin)]
+        self.b = Value(self.init_strategy())
         self.activation = activation
 
     def __call__(self, x):
@@ -23,11 +33,35 @@ class Neuron:
             return output.sigmoid()
         elif self.activation == None:
             return output
+        
+    def _he_init_strategy(self, input_size: int):
+        return lambda : np.random.normal(0, math.sqrt(2.0 / input_size))
+    
+    def _xavier_init_strategy(self, input_size: int):
+        return lambda : np.random.uniform(-1.0 / math.sqrt(input_size), 1.0 / math.sqrt(input_size))
     
     def parameters(self):
         return self.w + [self.b]
     
-class Layer:
+    def __repr__(self) -> str:
+        return  f'Neuron(nin={self.nin},init_strategy={self.init_strategy},activation={self.activation})'
+
+    
+class BaseLayer(ABC):
+    @abstractmethod
+    def __call__(self, x) -> list[float] | float:
+        pass
+
+    @abstractmethod
+    def parameters(self) -> list[float]:
+        pass
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        pass
+
+    
+class Layer(BaseLayer):
 
     def __init__(self, nin, nout, activation: Activations, **kwargs):
         self.nin = nin
@@ -58,3 +92,36 @@ class MLP:
     
     def parameters(self):
         return [p for layer in self.layers for p in layer.parameters()]
+    
+class DropoutLayer(BaseLayer):
+
+    def __init__(self, proportion = 0.2, **kwargs):
+        self.proportion = proportion
+        self.training = True
+
+    def __call__(self, x):
+        if not self.training:
+            return x
+        
+        mask = np.random.choice(range(len(x)), size=round(len(x) * (1 - self.proportion)), replace=False)
+        outs =  [x[i] if i in mask else 0.0 for i in range(len(x))]
+        return outs
+    
+    def parameters(self):
+        return []
+    
+    def __repr__(self) -> str:
+        return f'Dropout(proportion={self.proportion})'
+    
+class Sequential:
+    def __init__(self, layers: list[BaseLayer]):
+        self.layers = layers
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)     
+        return x
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+    
